767417e374d52beecb64ac5b8544d1574fa3ca8d
Abrarhasnat1989 <136226830+Abrarhasnat1989@users.noreply.github.com>
2023-09-03 10:41:41 +0200

    Add files via upload

---
+++ todo_list_project/todo_list/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class TaskModel(models.Model):
+    taskTitle = models.CharField(max_length=100)
+    taskDescription = models.TextField()
+    is_completed = models.BooleanField(default=False)
